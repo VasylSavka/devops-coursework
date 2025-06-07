efc4f6d1755d9583ea2736970ac721727257cbd9
Vasyl Savka <grumpy.ancient@gmail.com>
2025-06-07 22:30:10 +0300

    Fix notify_destroy.sh to export MESSAGE and WEBHOOK_URL

--- terraform/notify_destroy.sh
+++ terraform/notify_destroy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
 MESSAGE="Terraform destroy: EC2 instance is being terminated."
 WEBHOOK_URL="$1"
 
-MESSAGE="$MESSAGE" WEBHOOK_URL="$WEBHOOK_URL" ./teams_notify.sh
+export MESSAGE
+export WEBHOOK_URL
+
+./teams_notify.sh
+exit 0
