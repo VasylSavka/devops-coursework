07d21165e06ccd14f0bd3b2b286759303c744f88
Vasyl Savka <grumpy.ancient@gmail.com>
2025-06-08 09:20:26 +0300

    replace useles push notify

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,7 +28,6 @@ resource "null_resource" "teams_notify_apply" {
 resource "null_resource" "teams_notify_destroy" {
   triggers = {
     instance_id = aws_instance.devops_app.id
-    webhook_url = var.teams_webhook_url
   }
 
   provisioner "local-exec" {
