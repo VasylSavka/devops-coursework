9293d36f5ec3ac1be96d27ea712d9e01920333a1
Vasyl Savka <grumpy.ancient@gmail.com>
2025-06-08 17:55:59 +0300

    delete prev action_flag

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,13 +27,13 @@ resource "null_resource" "teams_notify_apply" {
 
 resource "null_resource" "teams_notify_destroy" {
   triggers = {
-    always_run  = timestamp()
-    webhook_url = var.teams_webhook_url
-    action_flag = var.action != null ? var.action : "destroy"
+    always_run   = timestamp()
+    webhook_url  = var.teams_webhook_url
+    action_flag  = var.action != null ? var.action : "unknown"
   }
 
   provisioner "local-exec" {
-    when    = destroy
+    when = destroy
     command = "bash ./destroy_logic.sh \"${self.triggers.action_flag}\" \"${self.triggers.webhook_url}\""
   }
 }
